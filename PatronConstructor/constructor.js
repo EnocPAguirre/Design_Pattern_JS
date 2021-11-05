c254200941cf3df28713fb51c929eb60a143cb57
enoc pineda <enocpinedaa@gmail.com>
2021-11-05 01:34:43 -0600

    Design Pattern

---
+++ PatronConstructor/constructor.js
@@ -0,0 +1,11 @@
+class MiClase {
+    constructor(p1) {
+        this.propiedad = p1
+        this.metodo = () => {
+
+        }
+    }
+}
+
+const instancia = new MiClase(4)
+console.log(instancia)
